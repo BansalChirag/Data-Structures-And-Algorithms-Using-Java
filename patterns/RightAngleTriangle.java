689c7bc16cc902cbbd9ae9e091394336a1316b1d
Chirag Bansal <bansalchirag091@gmail.com>
2025-06-29 15:33:11 +0530

    Right Angle Triangle Pattern

---
+++ patterns/RightAngleTriangle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class RightAngleTriangle {
+
+    void printTriangle(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        new RightAngleTriangle().printTriangle(n);
+        sc.close();
+    }
+}
