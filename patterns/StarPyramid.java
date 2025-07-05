3765879e2ad2b7412808305052a4d04f12b01205
Chirag Bansal <bansalchirag091@gmail.com>
2025-07-05 13:50:54 +0530

    Star Pyramid Pattern

--- patterns/StarPyramid.java
+++ patterns/StarPyramid.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
-class RightAngleTriangle {
-
-    void printTriangle(int n) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i + 1; j++) {
+public class StarPyramid {
+    void printStarPyramid(int n){
+        for (int rows = 0; rows < n; rows++) {
+            for (int spaces = 0; spaces < n-rows; spaces++) {
+                System.out.print(" ");
+            }
+            for (int star = 0; star < 2*rows+1; star++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of n: ");
         int n = sc.nextInt();
-        new RightAngleTriangle().printTriangle(n);
+        new StarPyramid().printStarPyramid(n);
         sc.close();
     }
 }
